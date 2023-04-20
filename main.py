cfb27b55f4c2b217529d4b26a56c1f7641c8eca8
JaTadd <jean-alexis.taddei@efrei.net>
2023-04-20 11:53:08 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Chargement des données
+Df_X = pd.read_csv('Data/Data_X.csv')
+Df_Y = pd.read_csv('Data/Data_Y.csv')
+
+# Fusion des ensembles de données
+DfNew_X = pd.merge(Df_X, Df_Y)
+
+# Selection d'un sous-ensemble ici Country
+print(DfNew_X['COUNTRY'])
+
+# Calcul de nouveaux attributs
+DfNew_X['FR_cons - net_import'] = DfNew_X['FR_CONSUMPTION'] - \
+    DfNew_X['FR_NET_IMPORT']
+print('FR_cons - net_import', DfNew_X['FR_cons - net_import'])
+
+# Trie des datas
+DfNew_X = DfNew_X.sort_values()
